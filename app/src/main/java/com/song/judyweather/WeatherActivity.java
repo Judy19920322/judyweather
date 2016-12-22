ac033b889cdad04c4a77a7cff8fd2fa5a3d243cb
Judy19920322 <522483446@qq.com>
2016-12-22 17:22:49 +0800

    天气界面完成，aqi有bug

---
+++ app/src/main/java/com/song/judyweather/WeatherActivity.java
@@ -0,0 +1,10 @@
+package com.song.judyweather;
+
+import android.app.Activity;
+
+/**
+ * Created by Judy on 2016/12/22.
+ */
+
+public class WeatherActivity extends Activity {
+}
---
+++ app/src/main/java/com/song/judyweather/gson/Suggestion.java
@@ -0,0 +1,8 @@
+package com.song.judyweather.gson;
+
+/**
+ * Created by Judy on 2016/12/22.
+ */
+
+public class Suggestion {
+}
